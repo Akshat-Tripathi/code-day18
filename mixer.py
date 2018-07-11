822d94aed041fadf648f19e4ace0f8b5542f64af
DerpyShadowz <DerpyShadowz@users.noreply.github.com>
2018-07-11 14:45:44 +0100

    Update mixer.py

--- mixer.py
+++ mixer.py
@@ -5,12 +5,14 @@ pygame.mixer.init()
 
 sound1 = pygame.mixer.Sound('Garden Music.mp3')
 sound2 = pygame.mixer.Sound('Floating Cities.mp3')
-sound1.play(loops=-1,fade_ms=1000)
+channel1 = pygame.mixer.Channel(1)
+channel2 = pygane.mixer.Channel(2)
+channel1.play(sound1,loops=-1,fade_ms=1000)
 
-time.sleep(10)
+time.sleep(20)
 
-sound1.fadeout(1000)
-sound2.play(loops=-1,fade_ms=1000)
+channel1.fadeout(1000)
+channel1.play(loops=-1,fade_ms=1000)
 
 
 # Test if this works plz :)
